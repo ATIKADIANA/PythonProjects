b2176c692ed454643d4918adf5bf99a55889b18a
atikadiana <atikadiana333@gmail.com>
2023-07-26 19:59:38 +0300

    book py updates

--- book.py
+++ book.py
@@ -1,1 +1,19 @@
 print('Diana')
+# guessing game 
+from random import randint
+from IPython.display import clear_output
+guessed = False
+number = 3
+guesses = 0
+while not guessed:
+# ans = input("Try to guess the number I am thinking of!")
+# guesses += 1
+# clear_output()
+# if int(ans) == 3
+## print("Congrats! You guessed it correctly.")
+## print( "It took you {} guesses!".format(guesses) )
+## break
+# elif int(ans) > 3
+## print("The number is lower than you guessed.")
+# elif int(ant) < 3
+## print("The number is greater than you guessed.")
